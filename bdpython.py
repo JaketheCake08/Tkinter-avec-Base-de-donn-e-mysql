3b4c9e2b4b5f6e4956df8f57cd6266a4fcc6b158
JaketheCake08 <111555440+JaketheCake08@users.noreply.github.com>
2022-09-30 16:16:22 -0400

    Add files via upload

---
+++ bdpython.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+host="localhost",
+user="root",
+password = "root1234"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE tkinterdb1")
